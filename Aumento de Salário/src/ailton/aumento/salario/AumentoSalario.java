abc0c122f95147dca340a4f74036c19756432e5f
Ailton <ailtonc.n64@gmail.com>
2022-04-19 12:13:16 -0300

    some changes

--- Aumento de Salário/src/ailton/aumento/salario/AumentoSalario.java
+++ Aumento de Salário/src/ailton/aumento/salario/AumentoSalario.java
@@ -1,19 +1,22 @@
 package ailton.aumento.salario;
-import java.util.Scanner;
-public class AumentoSalario {
 
+import java.util.Scanner;
+
+public class AumentoSalario {
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+		Scanner in = new Scanner(System.in); // double porcentagem = 15.0 / 100.0; // aumento de salário por 15%
 
 		double salario, salarioFinal;
-		double porc = 15.0 / 100.0;
+		double porcentagem;
 
-		System.out.println("Insira seu sal�rio: ");
-		salario = in.nextDouble(); // recebe o sal�rio sem o aumento
+        System.out.println("");
+        porcentagem = in.double
+		
+		System.out.println("Insira o salário a ser aumentado:");
+		salario = in.nextDouble(); // recebe o salário sem o aumento
 
-		salarioFinal = salario + (porc * salario);
-
-		System.out.println("Sal�rio com aumento: " + salarioFinal);
+		salarioFinal = salario + (porcentagem * salario);
+		
+		System.out.println("Salário com aumento: " + salarioFinal);
 	}
-
 }
