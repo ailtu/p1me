58681420592e8f9958cea7cb64beb10d15e8ce62
Ailton <ailtonc.n64@gmail.com>
2022-04-19 16:11:32 -0300

    AumentoSalario Indentation

--- Aumento de Salário/src/ailton/aumento/salario/AumentoSalario.java
+++ Aumento de Salário/src/ailton/aumento/salario/AumentoSalario.java
@@ -4,19 +4,16 @@ import java.util.Scanner;
 
 public class AumentoSalario {
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in); // double porcentagem = 15.0 / 100.0; // aumento de salário por 15%
+		Scanner in = new Scanner(System.in);
 
 		double salario, salarioFinal;
-		double porcentagem;
+		double porcentagem = 15.0 / 100.0; // aumento de salário por 15%
 
-        System.out.println("");
-        porcentagem = in.double
-		
-		System.out.println("Insira o salário a ser aumentado:");
+		System.out.println("Insira o salário a ser aumentado: ");
 		salario = in.nextDouble(); // recebe o salário sem o aumento
 
 		salarioFinal = salario + (porcentagem * salario);
-		
+
 		System.out.println("Salário com aumento: " + salarioFinal);
 	}
 }
